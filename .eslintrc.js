9e807b68737450bc435bf4ab88879dff756118ea
flywithpunitt <punitkumar9168@gmail.com>
2024-11-25 05:39:09 +0530

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
 module.exports = {
-    extends: ['airbnb-typescript'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: './tsconfig.json', // Ensure this path matches your `tsconfig.json` location
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'next/core-web-vitals',
+    ],
     rules: {
-      '@typescript-eslint/no-explicit-any': 'warn', // Only warn for `any`
+      '@typescript-eslint/dot-notation': 'off', // Optionally disable this rule
     },
   };
   
