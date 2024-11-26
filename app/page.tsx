e0f78f60b29a98f31813c92a10d34d9f23603a7a
flywithpunitt <punitkumar9168@gmail.com>
2024-11-26 16:11:19 +0530

    new change

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,9 @@
 "use client";
 
-import { navItems } from "@/data";
-
-import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
-import Footer from "@/components/Footer";
-import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import AboutSection from "@/components/AboutSection";
-import Education from "@/components/Education";
-
-
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
-        <Hero />
-        <AboutSection />
-        <Grid />
-        <RecentProjects />
-        <Education />
-        <Footer />
-      </div>
+    <main>
+      <h1>its Comingggggggggggg !!</h1>
     </main>
   );
 };
