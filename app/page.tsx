48a707b783c45b46eb1aabb3872e6d7cc9106aaa
flywithpunitt <punitkumar9168@gmail.com>
2024-11-26 16:15:26 +0530

    new changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+
 const Home = () => {
   return (
-    <main>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+              <Hero />
       <h1>its Comingggggggggggg !!</h1>
     </main>
   );
